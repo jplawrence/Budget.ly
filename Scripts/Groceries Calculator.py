19499d0618320485ab2106c352e67d3eaedd98d2
Jp <jplawrenca543@gmail.com>
2020-08-27 16:50:06 +0200

    Product and Price Detection
    User can now input products that are listed, and can also decide how many of these products they want. When the product number is satisfied, the totals will be outputted according to the products they have entered.

--- Scripts/Groceries Calculator.py
+++ Scripts/Groceries Calculator.py
@@ -29,3 +29,24 @@ bacon = Product(4.5)
 nesquik = Product(4.5)
 pasta = Product(1.5)
 takeouts = Product(15)
+
+a = int(input("How many items would you like to add to your cart? "))
+items_in_cart = 0
+cart_total = []
+reached = False
+while items_in_cart < a:
+    item = input("Add item: ")
+    if item == "eggs" and not reached:
+        eggs_item = True
+        items_in_cart += 1
+        print(eggs.price)
+        cart_total.append(eggs.price)
+    elif item == "milk" and not reached:
+        milk_item = True
+        items_in_cart += 1
+        print(milk.price)
+        cart_total.append(milk.price)
+
+
+print("Items in cart: " + str(items_in_cart))
+print("Total: $" + str(round(sum(cart_total))))
