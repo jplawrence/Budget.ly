b99fb6c78a9b35bcb17d46c30059e481665fb182
Jp <jplawrenca543@gmail.com>
2020-09-21 14:50:57 +0200

    Everything is done! Might be a few minor casualties, but everything is done! Added grocery calculator app, and adjusted totals.

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,3 +1,4 @@
+
 monthly_income = 0
 
 print("""
@@ -74,7 +75,7 @@ elif response == "b":
     total = sum(list_b)
     leftovers = monthly_income - total
 
-    print(f""""
+    print(f"""
 This is what we suggest: 
 Rent: ${rent}
 Car lease: ${car_lease}
@@ -89,3 +90,84 @@ Leftovers: ${leftovers}
 
 else:
     print("You didn't select an option!")
+
+response_food = input("Would you like to use our shopping app? (y) Yes, (n) No: ")
+if response_food == "y":
+    print("Please select items for weekly shopping. Your grocery allowance will be updated")
+    dictionary = {
+        "eggs": 1.5,
+        "milk": 2.5,
+        "bread": 3,
+        "cheese": 2.9,
+        "butter": 3,
+        "burgers": 8,
+        "fish fingers": 5,
+        "apples": 5,
+        "bananas": 1.5,
+        "chicken Fillets": 8.9,
+        "lamb": 10,
+        "pork": 7,
+        "chips": 2,
+        "oil": 2,
+        "lettuce": 2,
+        "rolls": 3.5,
+        "ketchup": 3,
+        "viennas": 3.5,
+        "juice": 2,
+        "water": 1,
+        "mince": 10,
+        "bacon": 4.5,
+        "pasta": 4.5,
+        "nesquik": 1.5,
+        "takeouts": 15,
+    }
+
+    total_prices = []
+
+    a = " "
+    while a != "":
+        item = input("Add Item: ")
+
+        if item in dictionary.keys():
+            item_price = dictionary.get(item)
+            total_prices.append(item_price)
+        else:
+            print("Sorry, we don't have that item")
+
+        if item == "":
+            break
+
+    print(f"""Order Summary:
+Total Items: {len(total_prices)}
+Total: ${sum(total_prices)}
+    """)
+
+    total_end_amount = sum(total_prices)
+    food = total_end_amount * 4
+
+    list_c = []
+
+    list_c.append(food)
+    list_c.append(rent)
+    list_c.append(car_lease)
+    list_c.append(fuel)
+    list_c.append(credit_expense)
+    list_c.append(savings)
+
+    print(f"""
+Updated Budget: 
+----
+Rent: ${rent}
+Car lease: ${car_lease}
+Fuel: ${fuel}
+Food: ${food}
+Credit Expense: ${credit_expense}
+Savings: ${savings}
+----
+Total: ${sum(list_c)}
+Leftovers: ${monthly_income - sum(list_c)}
+    """)
+
+else:
+    print("Thank you for using our app!")
+
